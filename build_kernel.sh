9ecf1fe5cc2c5f28395af1162777ba1b05f42b0d
Travis Tabbal <travis@tabbal.net>
2011-05-04 22:50:37 -0600

    Add build scripts, fix .gitignore. Add .config and a defconfig.

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,7 @@ export KBUILD_BUILD_VERSION="Dragon-v6.0-TEST1"
 
 make arch=arm -j4 prepare
 make arch=arm -j4 modules
-#find . -name *.ko -exec cp {} ../initramfs-voodoo/lib/modules/ \;
+find . -name *.ko -exec cp {} ./usr/initramfs/lib/modules/ \;
 make arch=arm -j4
 cp arch/arm/boot/zImage .
 ls -lh zImage
--- clean_build_kernel.sh
+++ clean_build_kernel.sh
@@ -5,7 +5,8 @@ export KBUILD_BUILD_VERSION="Dragon-v6.0-TEST1"
 make -j4 clean
 make -j4 prepare
 make -j4 modules
-#find . -name *.ko -exec cp {} ../initramfs-voodoo/lib/modules/ \;
+#find . -name *.ko -exec cp {} usr/initramfs/lib/modules/ \;
+find . -name *.ko -exec cp {} ./usr/initramfs/lib/modules/ \;
 make -j4
 cp arch/arm/boot/zImage .
 ls -lh zImage
